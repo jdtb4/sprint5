b48f92161a1b21be725e910f307dfde867d90565
Juan Trujillo <juantrujillobrinas@gmail.com>
2022-05-18 13:03:55 +0200

    Exercise 1

---
+++ app.js
@@ -0,0 +1,15 @@
+const printJokes = document.getElementById("jokePrint");
+
+async function callAPI(){
+    const jokeData =  await fetch ('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+    const jokeObj = await jokeData.json();
+
+    
+    console.log(jokeObj); // Exercise 1.
+    //printJokes.innerHTML = jokeObj.joke;
+}
+callAPI();
