5fd10811e81f1f84ec8b1cbeb1c8c6eec9e1a0d6
Juan Trujillo <juantrujillobrinas@gmail.com>
2022-05-24 09:29:28 +0200

    Previous Layout

--- app.js
+++ app.js
@@ -13,3 +13,30 @@ async function callAPI(){
     printJokes.innerHTML = jokeObj.joke; // Exercise 2.
 }
 callAPI();
+
+const reportJokes = [];
+let score = 0;
+let joke = callAPI()
+function vote(value){
+    const currentDate = new Date();
+    let dateISO = currentDate.toISOString();
+    if (value === 1){
+        score = 1
+    }
+    if (value === 2){
+        score = 2
+    }
+    if (value === 3){
+        score = 3
+    }
+    reportJokes.push({joke: joke.joke, date: dateISO, score: score.score})
+    console.log(reportJokes);
+    return score;
+}
+
+
+
+async function getWeather(){
+    const weatherData = await fetch('https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=d8ec460060f73b8a21776452facfeeec&lang=ca&units=metric');
+    let resWeatherData = await weatherData.json();
+}
