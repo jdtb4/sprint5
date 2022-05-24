27f4e80fc8af734bc84f3cf649b1182be7b35efa
Juan Trujillo <juantrujillobrinas@gmail.com>
2022-05-24 12:47:23 +0200

    Exercise 4

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ async function callAPI(){
     });
     const jokeObj = await jokeData.json();
     jokes = jokeObj
-    
     console.log(jokeObj); // Exercise 1.
     printJokes.innerHTML = jokeObj.joke; // Exercise 2.
     return jokeObj;
@@ -24,10 +23,15 @@ function vote(value){
     console.log(reportJokes);
     return score;
 }
-
-
+const printWeather = document.getElementById("temperature");
+const printConditions = document.getElementById("conditions")
 
 async function getWeather(){
     const weatherData = await fetch('https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=d8ec460060f73b8a21776452facfeeec&lang=ca&units=metric');
-    let resWeatherData = await weatherData.json();
+    const resWeatherData = await weatherData.json();
+    
+    let icons = "http://openweathermap.org/img/wn/" + resWeatherData.weather[0].icon + "@2x.png";
+    printConditions.innerHTML= `<img src="${icons}">`;
+    printWeather.innerHTML = resWeatherData.main.temp + "ºC";
 }
+getWeather();
